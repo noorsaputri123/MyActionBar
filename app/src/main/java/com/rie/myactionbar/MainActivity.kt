0cdd37fff47a367e15af8790f7199e3074856995
noorsaputri123 <c030320077@mahasiswa.poliban.ac.id>
2023-03-29 10:23:03 +0700

    berhasil membuat fitur search

--- app/src/main/java/com/rie/myactionbar/MainActivity.kt
+++ app/src/main/java/com/rie/myactionbar/MainActivity.kt
@@ -1,32 +1,64 @@
 package com.rie.myactionbar
 
 import android.content.Context
+
+import com.rie.myactionbar.databinding.ActivityMainBinding
+import android.app.SearchManager
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.rie.myactionbar.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 
-
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /*
+            Gunakan method ini ketika search selesai atau OK
+             */
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            /*
+            Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+             */
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu1 -> {
+
+                /*
+                Ketika menu ditekan maka akan memunculkan fragment,
+                fragment di tambahkan ke dalam backstack sehingga ketika ditekan back akan kembali ke MainActivity
+                 */
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, MenuFragment())
                     .addToBackStack(null)
@@ -41,6 +73,4 @@ class MainActivity : AppCompatActivity() {
             else -> return true
         }
     }
-
-
 }
